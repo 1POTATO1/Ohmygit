6bdeca44f874f396d886954ac558e4770ee581fc
1POTATO1 <38481771+1POTATO1@users.noreply.github.com>
2018-04-22 14:47:33 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function paragraph_onmouseover(){
+  yourstyle.style.display="";
+}
+
+function paragraph_onmouseout(){
+  yourstyle.style.display="none";
+}
+function paragraph1_onmouseover(){
+  yourstyle1.style.display="";
+}
+
+function paragraph1_onmouseout(){
+  yourstyle1.style.display="none";
+}
+function paragraph2_onmouseover(){
+  yourstyle2.style.display="";
+}
+
+function paragraph2_onmouseout(){
+  yourstyle2.style.display="none";
+}
+function paragraph3_onmouseover(){
+  yourstyle3.style.display="";
+}
+
+function paragraph3_onmouseout(){
+  yourstyle3.style.display="none";
+}
+function paragraph4_onmouseover(){
+  yourstyle4.style.display="";
+}
+
+function paragraph4_onmouseout(){
+  yourstyle4.style.display="none";
+}
